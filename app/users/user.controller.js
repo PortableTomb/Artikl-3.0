9c71f4d2a43604d906437a06f14248c8ab20fd09
PortableTomb <tenderyoungpony@gmail.com>
2017-01-01 18:52:57 -0800

    sidebar users search function happening

--- app/users/user.controller.js
+++ app/users/user.controller.js
@@ -2,6 +2,11 @@ class UserCtrl {
   constructor(userService) {
     this.userService = userService;
     this.searchtext = '';
+    this.users = [];
+
+    this.filterFunction = function(element) {
+      return element.username.match(/^Ma/) ? true : false;
+    };
   }
 
   getAllUsers() {
